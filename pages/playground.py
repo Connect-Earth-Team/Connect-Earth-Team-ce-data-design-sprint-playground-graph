12f5c0c04ae3606798686493bfbc8c28813d509b
josh-connect-earth <josh@connect.earth>
2025-04-10 11:43:55 +0100

    setup

--- pages/playground.py
+++ pages/playground.py
@@ -1,8 +1,18 @@
 import streamlit as st
 
+import utils.display as display
+import utils.inputs as inputs
+
 
 def main():
     st.title("Playgrounnnndddd")
+
+    st.title("Display")
+    display.main()
+
+
+    st.title("Inputs")
+    inputs.main()
 
 
 if __name__ == "__main__":
