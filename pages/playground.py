6751a237393d0788e618ad7aea9f3cf30ec5db10
josh-connect-earth <josh@connect.earth>
2025-04-10 13:08:40 +0100

    morning progress

--- pages/playground.py
+++ pages/playground.py
@@ -4,13 +4,15 @@ import streamlit as st
 import utils.display as display
 import utils.inputs as inputs
 
-data_raw = pd.read_csv("data/elec_consumption.csv")
+data_raw = pd.read_csv("data/elec_consumption.csv", index_col=0)
+
+st.dataframe(data_raw)
 
 def main():
     st.title("Playgrounnnndddd")
 
     st.title("Inputs")
-    inputs.main()
+    inputs.main(data_raw=data_raw)
 
     st.title("Display")
     display.main()
